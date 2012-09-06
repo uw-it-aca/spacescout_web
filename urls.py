5199bb3f0e2182d89aa84422f8ad347964da4e35
Craig M. Stimmel <cstimmel@uw.edu>
2012-09-06 16:21:47 -0700

    Moved app name to 'spacescout_web'

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'spotseeker_web.views.home.HomeView'),
-    url(r'search/$', 'spotseeker_web.views.search.SearchView'),
-    url(r'spot/(?P<spot_id>\d+)$', 'spotseeker_web.views.spot.SpotView'),
+    url(r'^$', 'spacescout_web.views.home.HomeView'),
+    url(r'search/$', 'spacescout_web.views.search.SearchView'),
+    url(r'spot/(?P<spot_id>\d+)$', 'spacescout_web.views.spot.SpotView'),
 )
