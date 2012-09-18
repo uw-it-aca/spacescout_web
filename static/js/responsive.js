2a7bd60e0622c75ea2afc5f49344b875c3972dc7
Charlon Palacay <charlon@gmail.com>
2012-09-18 11:30:38 -0700

    updates timing of details view behavior

--- static/js/responsive.js
+++ static/js/responsive.js
@@ -118,14 +118,12 @@
 
         // handle view details click
         $('.view-details').live('click', function(e){
-            console.log('clickasdfasfd fired');
             e.preventDefault();
             showSpaceDetails();
         });
 
         $('#space_detail_container .close').live('click', function(e){
             e.preventDefault();
-
             hideSpaceDetails();
         });
 
@@ -140,21 +138,45 @@
 	function showSpaceDetails() {
 
     	// remove any open details
-    	$('#space_detail_container').remove();
+    	//$('#space_detail_container').remove();
 
     	if (!mobile) { // if desktop
-
-    	   console.log('show space function fired');
 
     	   // build the template
     	   var source = $('#space_details').html();
     	   var template = Handlebars.compile(source);
     	   $('#map_canvas').append(template(template));
 
+    	   // set/reset initial state
+    	   $('.space-detail-inner').hide();
+    	   $(".space-detail .loading").show();
+
     	   // display behavior
     	   $('#space_detail_container').height($('#map_canvas').height());
-    	   $('#space_detail_container').show();
-    	   $('.space-detail').slideDown('slow');
+
+    	   if ($('#space_detail_container').is(':visible')) {
+
+        	   console.log("fire again... container already open");
+
+        	   setTimeout('$(".space-detail .loading").hide()', 1000);
+        	   setTimeout('$(".space-detail-inner").show()', 1500);
+
+    	   }
+    	   else {
+        	   // open the container
+        	   $('#space_detail_container').show();
+
+        	   // if space container is open, just show
+        	   $('.space-detail').slideDown('slow', function() {
+
+            	   setTimeout('$(".space-detail .loading").hide()', 1000);
+            	   setTimeout('$(".space-detail-inner").show()', 1500);
+
+        	   });
+
+    	   }
+
+
 	   }
 	   else { // TODO: mobile should open new page
     	   console.log('do something else for mobile -- open new page');
