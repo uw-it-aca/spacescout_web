e200ce83d7de2a22e0cb2ad05625ee557dfd35cb
Craig M. Stimmel <cstimmel@uw.edu>
2012-09-24 14:25:40 -0700

    Added a way to get back the json data for 1 space.

--- urls.py
+++ urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r'^$', 'spacescout_web.views.home.HomeView'),
     url(r'search/$', 'spacescout_web.views.search.SearchView'),
     url(r'space/(?P<spot_id>\d+)$', 'spacescout_web.views.spot.SpotView'),
+    url(r'space/(?P<spot_id>\d+)/json/$', 'spacescout_web.views.spot.SpotView', {'return_json':True}),
 )
