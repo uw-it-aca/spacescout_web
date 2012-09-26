ab218bfec95afdde29c2b4e76e9e152f7c45188c
Charlon Palacay <charlon@gmail.com>
2012-09-26 15:27:41 -0700

    yay for handlebars compare helper

--- static/js/responsive.js
+++ static/js/responsive.js
@@ -11,6 +11,38 @@ Handlebars.registerHelper('carouselimages', function(spacedata) {
     }
 });
 
+Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
+
+    if (arguments.length < 3)
+        throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+
+    operator = options.hash.operator || "==";
+
+    var operators = {
+        '==':       function(l,r) { return l == r; },
+        '===':      function(l,r) { return l === r; },
+        '!=':       function(l,r) { return l != r; },
+        '<':        function(l,r) { return l < r; },
+        '>':        function(l,r) { return l > r; },
+        '<=':       function(l,r) { return l <= r; },
+        '>=':       function(l,r) { return l >= r; },
+        'typeof':   function(l,r) { return typeof l == r; }
+    }
+
+    if (!operators[operator])
+        throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+
+    var result = operators[operator](lvalue,rvalue);
+
+    if( result ) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+
+});
+
+
 (function(w){
 	var sw = document.body.clientWidth,
 		breakpoint = 767,
