1c9e057e464ece64bd8192239ecba0e007b68ed0
Mouse Reeve <mreeve@uw.edu>
2012-10-04 11:34:51 -0700

    Formats to 12 hours time and displays days in the correct order

--- static/js/responsive.js
+++ static/js/responsive.js
@@ -50,17 +50,47 @@ Handlebars.registerHelper('formatHours', function(hours) {
     //if (start_time[0] == 0 && start_time[1] == 0 && end_time[0] == 23 && end_time[1] == 59 && tomorrow_starts_at_midnight && !tomorrow_is_24_hours && tomorrows_hour > 3) {
         //dsf
     //}
-    var formatted = "";
+    var formatted = [];
     $.each(hours, function(day) {
         dayMarker = day.charAt(0);
         dayMarker = dayMarker.toUpperCase();
         if (dayMarker == 'T' && day.charAt(1) == 'h' || dayMarker == 'S' && day.charAt(1) == 'u') {
             dayMarker += day.charAt(1);
         }
-        formatted += "<p>" + dayMarker + ": " + hours[day] +  "</p>\n";
+        
+        formatted[dayMarker] = to12Hour(hours[day]);
     });
-    return new Handlebars.SafeString(formatted);
+    formatted = sortDays(formatted);
+    return new Handlebars.SafeString(formatted.join("<br/>"));
 });
+
+function to12Hour(day) {
+    var data = [ day[0][0], day[0][1] ];
+    for (var i=0; i<data.length; i++) {
+        time = data[i].split(":");
+        if (time[0] > 12) {
+            time[0] -= 12;
+            time[1] += "PM";
+        }
+        else if (time[0] < 1) {
+            time[0] = 12;
+            time[1] += "AM";
+        }
+        else {
+            time[1] += "AM";
+        }
+        data[i] = time.join(":");
+    }
+    return data[0] +" - " +data[1];
+}
+function sortDays(days) {
+    var ordered = [];
+    order = ["M", "T", "W", "Th", "F", "S", "Su"];
+    $.each(order, function(day) {
+        ordered.push(order[day] +": " +days[order[day]] );
+    });
+    return ordered;
+}
 
 (function(w){
 
