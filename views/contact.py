3574bc9e13535c3942fde688355e5ad24e20cdd3
Sam Olds <samolds@uw.edu>
2012-10-11 12:58:57 -0700

    committing some stuff so i can merge in new craig and char changes

--- views/contact.py
+++ views/contact.py
@@ -5,13 +5,15 @@ from django.core.mail import send_mail
 from web_proj.settings import FEEDBACK_EMAIL_RECIPIENT
 
 def ContactView(request):
+    back = '/'
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             sender = form.cleaned_data['sender']
             message = form.cleaned_data['message']
-            feedback_choice = form.cleaned_data['feedback_choice']
+            #feedback_choice = form.cleaned_data['feedback_choice']
+            feedback_choice = 'problem'
             bot_test = form.cleaned_data['email_confirmation']
 
             if 'spot_id' in request.session:
@@ -31,17 +33,31 @@ def ContactView(request):
 
             browser = request.META.get('HTTP_USER_AGENT', 'Unknown')
             subject = "SpaceScout %s from %s" %(feedback_choice, name)
-            email_message = "SpaceScout Web - %s \n\n %s \n\n %s - %s \n %s - ID = %s \
+            email_message = "SpaceScout Web - %s \n\n %s \n\n %s %s \n %s - ID = %s \
                 \n Browser Type = %s" %(feedback_choice, message, name, sender, spot_name, spot_id, browser)
 
             if bot_test == '':
-                send_mail(subject, email_message, sender, FEEDBACK_EMAIL_RECIPIENT)
+                try:
+                    send_mail(subject, email_message, sender, FEEDBACK_EMAIL_RECIPIENT)
+                except Exception as e:
+                    if e.errno == 61:
+                        return HttpResponseRedirect('/contact/sorry/')
 
-            if is_mobile and spot_id != 'Unknown':
-                return HttpResponseRedirect('/space/' + spot_id)
-            else:
-                return HttpResponseRedirect('/')
+            #if is_mobile and spot_id != 'Unknown':
+                #return HttpResponseRedirect('/space/' + spot_id)
+            #else:
+                #return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/contact/thankyou/')
     else:
         form = ContactForm()
 
     return render(request, 'contact.html', {'form': form})
+
+def ThankYouView(request):
+    back = '/'
+    return render(request, 'thankyou.html')
+
+def SorryView(request):
+    back = '/'
+    email = FEEDBACK_EMAIL_RECIPIENT
+    return render(request, 'sorry.html')
