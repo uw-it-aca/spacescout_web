46a0560a81cb74d8d6c45a856e1d59fdb60a7f75
Charlon Palacay <charlon@gmail.com>
2012-10-26 11:42:10 -0700

    resolves spot-322 adds carousel pagination

--- static/js/desktop.js
+++ static/js/desktop.js
@@ -264,6 +264,10 @@ Handlebars.registerHelper('ifany', function(a, b) {
             }
         });
 
+
+
+
+
 	});
 
 	// Update dimensions on resize
@@ -405,6 +409,57 @@ Handlebars.registerHelper('ifany', function(a, b) {
                 $(this).find('.carousel-control').hide();
             }
         });
+
+        // add carousel pagination
+        $('.carousel').each(
+                function() {
+                        var html = '<div class="carousel-nav" data-target="' + $(this).attr('id') + '"><ul>';
+
+                        for(var i = 0; i < $(this).find('.item').size(); i ++) {
+                                html += '<li><a';
+                                if(i == 0) {
+                                        html += ' class="active"';
+                                }
+
+                                html += ' href="#">•</a></li>';
+                        }
+
+                        html += '</ul></li>';
+                        $(this).before(html);
+                        $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').hide();
+                }
+        ).bind('slid',
+                function(e) {
+                        var nav = $('.carousel-nav[data-target="' + $(this).attr('id') + '"] ul');
+                        var index = $(this).find('.item.active').index();
+                        var item = nav.find('li').get(index);
+
+                        nav.find('li a.active').removeClass('active');
+                        $(item).find('a').addClass('active');
+
+                        if(index == 0) {
+                                $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').fadeOut();
+                        } else {
+                                $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').fadeIn();
+                        }
+
+                        if(index == nav.find('li').size() - 1) {
+                                $('.carousel-control.right[href="#' + $(this).attr('id') + '"]').fadeOut();
+                        } else {
+                                $('.carousel-control.right[href="#' + $(this).attr('id') + '"]').fadeIn();
+                        }
+                }
+        );
+
+        $('.carousel-nav a').bind('click',
+                function(e) {
+                        var index = $(this).parent().index();
+                        var carousel = $('#' + $(this).closest('.carousel-nav').attr('data-target'));
+
+                        carousel.carousel(index);
+                        e.preventDefault();
+                }
+        );
 
         resizeCarouselMapContainer();
 
--- static/js/mobile.js
+++ static/js/mobile.js
@@ -362,6 +362,57 @@ Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
             }
         });
 
+        // add carousel pagination
+        $('.carousel').each(
+                function() {
+                        var html = '<div class="carousel-nav" data-target="' + $(this).attr('id') + '"><ul>';
+
+                        for(var i = 0; i < $(this).find('.item').size(); i ++) {
+                                html += '<li><a';
+                                if(i == 0) {
+                                        html += ' class="active"';
+                                }
+
+                                html += ' href="#">•</a></li>';
+                        }
+
+                        html += '</ul></li>';
+                        $(this).before(html);
+                        $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').hide();
+                }
+        ).bind('slid',
+                function(e) {
+                        var nav = $('.carousel-nav[data-target="' + $(this).attr('id') + '"] ul');
+                        var index = $(this).find('.item.active').index();
+                        var item = nav.find('li').get(index);
+
+                        nav.find('li a.active').removeClass('active');
+                        $(item).find('a').addClass('active');
+
+                        if(index == 0) {
+                                $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').fadeOut();
+                        } else {
+                                $('.carousel-control.left[href="#' + $(this).attr('id') + '"]').fadeIn();
+                        }
+
+                        if(index == nav.find('li').size() - 1) {
+                                $('.carousel-control.right[href="#' + $(this).attr('id') + '"]').fadeOut();
+                        } else {
+                                $('.carousel-control.right[href="#' + $(this).attr('id') + '"]').fadeIn();
+                        }
+                }
+        );
+
+        $('.carousel-nav a').bind('click',
+                function(e) {
+                        var index = $(this).parent().index();
+                        var carousel = $('#' + $(this).closest('.carousel-nav').attr('data-target'));
+
+                        carousel.carousel(index);
+                        e.preventDefault();
+                }
+        );
+
     }
 
     function resizeFilterBlock() {
