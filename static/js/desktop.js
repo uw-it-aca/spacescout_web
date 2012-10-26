95e0cecf0714b666a5463c389b8b04e5b5ff6d5a
Charlon Palacay <charlon@gmail.com>
2012-10-26 15:10:11 -0700

    refactors carousel pagination and hides if only single image

--- static/js/desktop.js
+++ static/js/desktop.js
@@ -396,57 +396,53 @@ Handlebars.registerHelper('ifany', function(a, b) {
 
     function initializeCarousel() {
 
-        $('.carousel').each(function(){
+        // initialize the carousel
+        $('.carousel').each( function() {
+
             $(this).carousel({
                 interval: false
             });
 
+            // add carousel pagination
+            var html = '<div class="carousel-nav" data-target="' + $(this).attr('id') + '"><ul>';
+
+            for(var i = 0; i < $(this).find('.item').size(); i ++) {
+                html += '<li><a';
+                if(i == 0) {
+                    html += ' class="active"';
+                }
+
+                html += ' href="#">•</a></li>';
+            }
+
+            html += '</ul></li>';
+            $(this).before(html);
+
             //set the first item as active
             $(this).find(".item:first-child").addClass("active");
 
-            // hide the controls if only 1 picture exists
+            // hide the controls and pagination if only 1 picture exists
             if ($(this).find('.item').length == 1) {
                 $(this).find('.carousel-control').hide();
+                $(this).prev().hide(); // hide carousel pagination container for single image carousels
             }
+
+        }).bind('slid', function(e) {
+            var nav = $('.carousel-nav[data-target="' + $(this).attr('id') + '"] ul');
+            var index = $(this).find('.item.active').index();
+            var item = nav.find('li').get(index);
+
+            nav.find('li a.active').removeClass('active');
+            $(item).find('a').addClass('active');
         });
 
-        // add carousel pagination
-        $('.carousel').each(
-                function() {
-                        var html = '<div class="carousel-nav" data-target="' + $(this).attr('id') + '"><ul>';
+        $('.carousel-nav a').bind('click', function(e) {
+            var index = $(this).parent().index();
+            var carousel = $('#' + $(this).closest('.carousel-nav').attr('data-target'));
 
-                        for(var i = 0; i < $(this).find('.item').size(); i ++) {
-                                html += '<li><a';
-                                if(i == 0) {
-                                        html += ' class="active"';
-                                }
-
-                                html += ' href="#">•</a></li>';
-                        }
-
-                        html += '</ul></li>';
-                        $(this).before(html);
-                }
-        ).bind('slid',
-                function(e) {
-                        var nav = $('.carousel-nav[data-target="' + $(this).attr('id') + '"] ul');
-                        var index = $(this).find('.item.active').index();
-                        var item = nav.find('li').get(index);
-
-                        nav.find('li a.active').removeClass('active');
-                        $(item).find('a').addClass('active');
-                }
-        );
-
-        $('.carousel-nav a').bind('click',
-                function(e) {
-                        var index = $(this).parent().index();
-                        var carousel = $('#' + $(this).closest('.carousel-nav').attr('data-target'));
-
-                        carousel.carousel(index);
-                        e.preventDefault();
-                }
-        );
+            carousel.carousel(index);
+            e.preventDefault();
+        });
 
         resizeCarouselMapContainer();
 
