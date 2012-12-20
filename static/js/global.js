23d762af9369b74f310515423ce3d71148d2c3c0
Craig M. Stimmel <cstimmel@uw.edu>
2012-12-20 09:12:12 -0800

    Mobile needs to know the name of the location as well to be able to do the building search correctly.

--- static/js/global.js
+++ static/js/global.js
@@ -319,6 +319,7 @@ function preload(arrayOfImages) {
             window.default_latitude = $(this).val().split(',')[0];
             window.default_longitude = $(this).val().split(',')[1];
             window.default_location = $(this).val().split(',')[2];
+
             // in case a new location gets selected before the map loads
             if (window.spacescout_map != null) {
                 window.spacescout_map.setCenter(new google.maps.LatLng(window.default_latitude, window.default_longitude));
