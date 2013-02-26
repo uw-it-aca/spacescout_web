cd92fba7d09a2bab8e1a384eccdf040763267746
Craig M. Stimmel <cstimmel@uw.edu>
2013-02-26 08:34:32 -0800

    Added license notice to all UW-modified .py files.

--- forms/contact.py
+++ forms/contact.py
@@ -1,12 +1,27 @@
+""" Copyright 2012, 2013 UW Information Technology, University of Washington
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
 from django import forms
 
+
 class ContactForm(forms.Form):
-        name = forms.CharField(max_length = 25, label = "Your Name")
-        sender = forms.EmailField(max_length = 40, label = "Your Email (Optional)", required = False)
+        name = forms.CharField(max_length=25, label="Your Name")
+        sender = forms.EmailField(max_length=40, label="Your Email (Optional)", required=False)
         #feedback_choice = forms.ChoiceField((
             #('', 'Please Select One'),
             #('feedback', 'Leave Feedback'),
             #('problem', 'Report a Problem'),
             #('feature request', 'Request a Feature')), label="Feedback Choice")
-        message = forms.CharField(widget = forms.Textarea(), label = "Your Message")
-        email_confirmation = forms.CharField(required = False)
+        message = forms.CharField(widget=forms.Textarea(), label="Your Message")
+        email_confirmation = forms.CharField(required=False)
