44ce80f0099a04d4be65b6baa5ce43d71b69a721
Craig M. Stimmel <cstimmel@uw.edu>
2013-12-30 16:56:18 -0800

    Brought the filter analytics more in line with what we're doing on the iOS app.

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -6,8 +6,9 @@ var _gaq = _gaq || [];
  * It is possible that there is more than one tracker, hence this function 
  */  
  
-_ga.getEventTrackers_ = function(category, action, opt_label) {  
+_ga.getEventTrackers_ = function(category, action, opt_label) {
              
+    //console.log("cat: " + category + "; act: " + action + "; opt_l: " + opt_label);
     // we can return this anonnymous function and pass it to the _gaq 
     return function() {
         var trackers = _gat._getTrackers(); //Gets an array of allt he trackers from the _gat object
@@ -32,17 +33,17 @@ function trackCheckedFilters()  {
                         
     // get all checked checkboxes
     $('#filter_block [type="checkbox"]:checked').each(function () {
-        _gaq.push(_ga.getEventTrackers_("Filter", "Checked", this.id, this.value));
+        _gaq.push(_ga.getEventTrackers_("Filters", window.default_location+"-"+this.name, this.value));
     });
     
     // get all checked radio buttons
     $('#filter_block [type="radio"]:checked').each(function () {
-        _gaq.push(_ga.getEventTrackers_("Filter", "Checked", this.id, this.value));
+        _gaq.push(_ga.getEventTrackers_("Filters", window.default_location+"-"+this.name, this.value));
     });
     
     // TODO: get all selected dropdowns items
     /*$('#filter_block option:selected').each(function () {
-        _gaq.push(_ga.getEventTrackers_("Filter", "Selected", this.id, this.value));
+        _gaq.push(_ga.getEventTrackers_("Filters", window.default_location+"-"+this.name, this.value));
     })*/
     
 }
