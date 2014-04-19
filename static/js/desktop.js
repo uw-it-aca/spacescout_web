bf9f3321ee505c9ed5f4c9edb4a8e39c5b76131f
Mike Seibel <mikes@uw.edu>
2014-04-18 17:40:56 -0700

    adds desktop review/rating input

--- static/js/desktop.js
+++ static/js/desktop.js
@@ -29,6 +29,8 @@ Handlebars.registerHelper('ifany', function(a, b) {
 
 (function(d){
 
+    var REVIEW_CHAR_LIMIT = 300;
+
 	var sw = document.body.clientWidth,
 		breakpoint = 767,
 		speed = 600,
@@ -302,6 +304,76 @@ Handlebars.registerHelper('ifany', function(a, b) {
 
             fav_icon.tooltip({ placement: 'right', title: title});
         }
+
+        // setup reviews
+
+        $('.space-reviews .write-a-review').on('click', function (e) {
+            $('.space-reviews button.write-a-review').hide();
+            $('.space-review-compose').show(400);
+            $('.space-reviews-none').hide(400);
+            $('.space-review-rating div b span').html(REVIEW_CHAR_LIMIT);
+        });
+
+        $('.space-review-compose textarea').keyup(function (e) {
+            var remaining = REVIEW_CHAR_LIMIT - $(this).val().length;
+
+            if (remaining > 0) {
+                $('.space-review-rating div b span').html(remaining);
+            } else {
+                $('.space-review-rating div b span').html(0);
+            }
+        });
+
+        $('button#space-review-cancel').click(function (e) {
+            var node = $('.space-review-compose');
+
+            node.hide(400);
+            $('.space-reviews-none').show();
+            $('.space-reviews button.write-a-review').show();
+
+            $('textarea', node).val('');
+            $('.space-review-stars span', node).html('');
+            $('.space-review-rating div b span', node).html(REVIEW_CHAR_LIMIT);
+            $('.space-review-rating i', node).switchClass('fa-star', 'fa-star-o');
+        });
+
+        $('button#space-review-submit').click(function (e) {
+            $('.space-reviews button.write-a-review').hide();
+            $('.space-review-compose').hide(400);
+            $('.space-review-submitted').show();
+        });
+
+        $('.space-review-submitted a').click(function (e) {
+            $('.space-review-submitted').hide(400);
+            $('.space-reviews-none').show();
+            $('.space-reviews button.write-a-review').show();
+        });
+
+        $(document).on('click', '.space-review-stars i', function (e) {
+            var target = $(e.target),
+                stars = target.prevAll('i'),
+                rating = stars.length + 1,
+                ratings = ['', gettext('terrible'), gettext('poor'),
+                           gettext('average'), gettext('good'), gettext('excellent')];
+
+            stars.switchClass('fa-star-o', 'fa-star');
+            target.switchClass('fa-star-o', 'fa-star');
+            target.nextAll('i').switchClass('fa-star', 'fa-star-o');
+            $('.space-review-stars span').html(ratings[rating]);
+        });
+
+        $('.space-review-compose a').on('click', function (e) {
+            var target = $(e.target),
+                ul = target.next('ul');
+
+            if (ul.is(':visible')) {
+                target.find('i').switchClass('fa-angle-double-up', 'fa-angle-double-down');
+            } else {
+                target.find('i').switchClass('fa-angle-double-down', 'fa-angle-double-up');
+            }
+
+            ul.toggle(400);
+        });
 
         $('button#share_space').unbind('click');
         $('button#share_space').click(function (e) {
