043aec39f400d0060b1b06abd8a76d85741be004
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-12 12:50:26 -0800

    Restructuring to take a step closer to being setuptools compatible.

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ from distutils.core import setup
 setup(name='SpaceScout-Web',
       version='1.0',
       description='Web frontend for SpaceScout',
-      install_requires=['Django>=1.4,<1.5','oauth2','oauth_provider', 'django-compressor', 'django-mobility', 'django-templatetag-handlebars'],
+      install_requires=[
+                        'Django>=1.4,<1.5',
+                        'oauth2',
+                        'oauth_provider',
+                        'django-compressor',
+                        'django-mobility',
+                        'django-templatetag-handlebars'],
      )
