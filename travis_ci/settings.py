dab58f644cb703da58dd93ee249c66c94fb4466b
Craig M. Stimmel <cstimmel@uw.edu>
2016-02-19 13:24:00 -0800

    Add a secret key

--- travis_ci/settings.py
+++ travis_ci/settings.py
@@ -125,7 +125,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'secret-key-for-travis'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
